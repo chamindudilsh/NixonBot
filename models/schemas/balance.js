e2dc5817d2939d3d2cd18ac0626807753e0a6fe9
Chamindu Dilshan <53786153+chamindudilsh@users.noreply.github.com>
2022-03-05 14:47:10 +0530

    _id

--- models/schemas/balance.js
+++ models/schemas/balance.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const balanceSchema = new mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
     memberId: String,
     wallet: { type: Number, default: 2000 },
     bank: { type: Number, default: 0 }
